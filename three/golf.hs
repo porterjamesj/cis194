d08fbb25c048468dd6188d13f20f80997ce860d2
Ubuntu <porterjamesj@gmail.com>
2015-01-18 16:40:36 -0600

    add angry TODO

--- three/golf.hs
+++ three/golf.hs
@@ -3,6 +3,10 @@ module Golf where
 import Data.List
 import Data.List.Split
 
+-- TODO annoyed that this uses last
+--
+-- it's safe here because the result of chunksOf never contains an
+-- empty list, but still
 skipsN :: [a] -> Int -> [a]
 skipsN xs n = map last (dropWhileEnd (\x -> length x < n) split)
   where split = chunksOf n xs
