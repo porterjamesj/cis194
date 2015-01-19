c39f7c9c8c00310686659e90d0a0dae53d812d62
Ubuntu <porterjamesj@gmail.com>
2015-01-18 19:25:23 -0600

    localMaxima

--- three/golf.hs
+++ three/golf.hs
@@ -1,7 +1,12 @@
+
+
 module Golf where
 
 import Data.List
 import Data.List.Split
+import Data.Maybe
+
+-- skips
 
 -- TODO annoyed that this uses last
 --
@@ -13,3 +18,20 @@ skipsN xs n = map last (dropWhileEnd (\x -> length x < n) split)
 
 skips :: [a] -> [[a]]
 skips xs = map (skipsN xs) [1..(length xs)]
+
+-- localMaxima
+-- I am dissatisfied that windows is recursive, but otherwise pretty happy with this
+
+windows3 :: [a] -> [(a,a,a)]
+windows3 (x:x':x'':xs) = (x,x',x'') : windows3 (x':x'':xs)
+windows3 _ = []
+
+maxMiddle :: (Integer, Integer, Integer) -> Maybe Integer
+maxMiddle (a,b,c)
+  | b > a && b > c = Just b
+  | otherwise = Nothing
+
+localMaxima :: [Integer] -> [Integer]
+localMaxima xs = catMaybes $ map maxMiddle (windows3 xs)
+
+histogram :: [Integer] -> String
