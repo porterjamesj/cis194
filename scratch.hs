cd48f8669e4af4fd1057ae8b3519ff2b43ca7d05
James Porter <porterjamesj@gmail.com>
2016-04-18 15:43:50 -0400

    starting hw6

--- scratch.hs
+++ scratch.hs
@@ -1,6 +1,27 @@
-empty :: [a] -> [a]
-empty _ = []
+-- empty :: [a] -> [a]
+-- empty _ = []
 
-weird :: [a] -> [a]
-weird [] = []
-weird (x:xs) = [x]
+-- weird :: [a] -> [a]
+-- weird [] = []
+-- weird (x:xs) = [x]
+
+
+-- f1 :: Maybe a -> [Maybe a]
+-- f1 m = [m,m]
+
+-- f2 :: Maybe a -> [a]
+-- f2 Nothing = []
+-- f2 (Just x) = [x]
+
+-- the robber problem
+
+-- recursive edition (exponential)
+
+maxMoney :: [Int] -> Int
+naiveMaxMoney (x:xs) = max robIt skipIt
+  where
+    robIt = x + (naiveMaxMoney $ drop 1 xs)
+    skipIt = naiveMaxMoney xs
+naiveMaxMoney [] = 0
+
+-- dynamic programming edition (linear)
