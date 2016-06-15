0a97d482bcfb9abe45aac19ef9f2b422f147acab
James Porter <porterjamesj@gmail.com>
2016-06-15 00:42:14 -0400

    remove buggy definition of *> and all is well

--- eleven/hw11.hs
+++ eleven/hw11.hs
@@ -1,31 +1,18 @@
-import Control.Applicative hiding ((*>), mapA, sequenceA, replicateA)
+import Control.Applicative
 import Data.Char
-import Debug.Trace
 import AParser
-
-(*>) :: Applicative f => f a -> f b -> f b
-(*>) = \_ fb -> fb
-
-sequenceA :: Applicative f => [f a] -> f [a]
-sequenceA = foldr (liftA2 (:)) (pure [])
-
-mapA :: Applicative f => (a -> f b) -> ([a] -> f [b])
-mapA f = sequenceA . map f
 
 oneOrMore :: Parser a -> Parser [a]
 oneOrMore p = (:) <$> p <*> zeroOrMore p
 
-
 zeroOrMore :: Parser a -> Parser [a]
 zeroOrMore p = oneOrMore p <|> pure []
-
 
 spaces :: Parser String
 spaces = zeroOrMore $ satisfy isSpace
 
 ident :: Parser String
 ident = (:) <$> (satisfy isAlpha) <*> zeroOrMore (satisfy isAlphaNum)
-
 
 
 -- Parsing S Expressions
